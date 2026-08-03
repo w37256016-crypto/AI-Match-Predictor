3d8937d61499845f99031f217c000a02670368b1
w37256016-crypto <w37256016@gmail.com>
2026-08-04 00:59:06 +0100

    Update predictor.py

--- prediction/predictor.py
+++ prediction/predictor.py
@@ -1,14 +1,1 @@
-class MatchFeatures:
-    def __init__(self):
-        pass
 
-    def extract(self, match_data):
-        return {
-            "home_form": match_data.get("home_form", 0),
-            "away_form": match_data.get("away_form", 0),
-            "home_attack": match_data.get("home_attack", 0),
-            "away_attack": match_data.get("away_attack", 0),
-            "home_defense": match_data.get("home_defense", 0),
-            "away_defense": match_data.get("away_defense", 0),
-            "home_advantage": match_data.get("home_advantage", 1),
-        }
