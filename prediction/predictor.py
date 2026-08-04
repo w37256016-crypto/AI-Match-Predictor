c020f2d08422fcd4fcea6d987e2de5a066f9075f
w37256016-crypto <w37256016@gmail.com>
2026-08-04 08:38:40 +0100

    Update predictor.py

--- prediction/predictor.py
+++ prediction/predictor.py
@@ -2,9 +2,31 @@ from .model import MatchPredictionModel
 
 
 class MatchPredictor:
-
     def __init__(self):
         self.model = MatchPredictionModel()
 
-    def predict(self, match_data):
-        return self.model.predict(match_data)
+    def predict(
+        self,
+        league_id,
+        season,
+        fixture_id,
+        home_team_id,
+        away_team_id
+    ):
+        result = self.model.predict(
+            league_id,
+            season,
+            fixture_id,
+            home_team_id,
+            away_team_id
+        )
+
+        return {
+            "fixture_id": fixture_id,
+            "league_id": league_id,
+            "season": season,
+            "home_team_id": home_team_id,
+            "away_team_id": away_team_id,
+            "prediction": result,
+            "winner": max(result, key=result.get)
+        }
