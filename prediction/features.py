993413c268b1e359827f45ef6a822f496450ae4e
w37256016-crypto <w37256016@gmail.com>
2026-08-04 08:20:39 +0100

    Update features.py

--- prediction/features.py
+++ prediction/features.py
@@ -1,14 +1,31 @@
+from api.football_api import FootballAPI
+
+
 class MatchFeatures:
     def __init__(self):
-        pass
+        self.api = FootballAPI()
 
-    def extract(self, match_data):
+    def extract(self, league_id, season, fixture_id, home_team_id, away_team_id):
+        home_stats = self.api.get_team_statistics(
+            league_id, season, home_team_id
+        )
+
+        away_stats = self.api.get_team_statistics(
+            league_id, season, away_team_id
+        )
+
+        h2h = self.api.get_h2h(
+            home_team_id,
+            away_team_id
+        )
+
+        fixture_stats = self.api.get_fixture_statistics(
+            fixture_id
+        )
+
         return {
-            "home_form": match_data.get("home_form", 0),
-            "away_form": match_data.get("away_form", 0),
-            "home_attack": match_data.get("home_attack", 0),
-            "away_attack": match_data.get("away_attack", 0),
-            "home_defense": match_data.get("home_defense", 0),
-            "away_defense": match_data.get("away_defense", 0),
-            "home_advantage": match_data.get("home_advantage", 1),
+            "home_stats": home_stats,
+            "away_stats": away_stats,
+            "h2h": h2h,
+            "fixture_stats": fixture_stats
         }
