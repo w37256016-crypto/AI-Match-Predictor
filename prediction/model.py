9c4e2e3dee0bae8707f200a487cdc9ea75c1b0f4
w37256016-crypto <w37256016@gmail.com>
2026-08-04 01:08:32 +0100

    Update model.py

--- prediction/model.py
+++ prediction/model.py
@@ -1,1 +1,23 @@
+from .features import MatchFeatures
+from .scorer import MatchScorer
+from .probability import ProbabilityEngine
 
+
+class MatchPredictionModel:
+
+    def __init__(self):
+        self.features = MatchFeatures()
+        self.scorer = MatchScorer()
+        self.probability = ProbabilityEngine()
+
+    def predict(self, match_data):
+        features = self.features.extract(match_data)
+
+        strengths = self.scorer.calculate_strength(features)
+
+        probabilities = self.probability.calculate(
+            strengths["home_strength"],
+            strengths["away_strength"]
+        )
+
+        return probabilities
